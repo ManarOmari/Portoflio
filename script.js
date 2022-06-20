e0d0b365c8e67a94eb7d821297a784c6579434e9
Manar <manar.qasem732016@gmail.com>
2022-06-20 22:07:28 +0300

    add js file

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav(){
+  document.querySelector(".list-nav").style.display="block";
+
+    //document.querySelector(".flex-container").classList.toggle(".list-nav");
+   
+  }
+
+  function closeNav(){
+    document.querySelector('.list-nav').style.display = "none";
+    
+  }
