a406296ae79e4b734da1ed1a3114be815ea58a51
Manar <manar.qasem732016@gmail.com>
2022-06-21 18:17:13 +0300

    make two section responsive

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function openNav(){
-  document.querySelector(".list-nav").style.display="block";
+const hamburger= document.querySelector(".hamburger");
+const navMenu= document.querySelector(".list-nav");
 
-  }
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
 
-  function closeNav(){
-    document.querySelector('.list-nav').style.display = "none";
-    
-  }
+document.querySelectorAll(".list-nav").forEach(n =>n.addEventListener("click",()=>{
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
