82624307b036d55a2684a065ff38f4cab58e6c9c
Shuhad786 <shuhad.loofer@gmail.com>
2022-06-21 21:34:16 +0200

    update js file

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.list-nav');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
 });
 
-document.querySelectorAll('.list-nav').forEach(n =>n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
+document.querySelectorAll('.list-nav').forEach(n => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.remove('active');
 }));
