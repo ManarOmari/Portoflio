b443abdbf01f5444773fe30f8301e72320a614d9
Manar <manar.qasem732016@gmail.com>
2022-06-23 15:05:31 +0300

    fixed js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const modal = document.querySelector('.modal');
 let projectList = {
   project1: {
     name: 'Tonic',
-    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     featured_image: new Image(),
     technologies: ['html', 'css', 'javascript'],
     link_live_version: new URL('https://www.google.com'),
