1924f31e3c936d4004425bcd3723d5613ea1c36a
Manar <manar.qasem732016@gmail.com>
2022-06-23 18:10:09 +0300

    update js and html

--- vailidation-form.js
+++ vailidation-form.js
@@ -1,14 +1,12 @@
 const submitBtn = document.querySelector('.btn');
 const validateEmail = (email) => {
-  (email.match(/^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:.[a-z0-9-]+)*$/));
+  return (email.match( /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/));
 };
 
 submitBtn.addEventListener('click', () => {
   const emailVal = document.getElementById('mail').value;
-
+  
   if (!validateEmail(emailVal)) {
-    window.alert(
-      'Please enter email as lower case eg. manar.qasem732016@gmail.com',
-    );
+    document.querySelector('.txtt').innerHTML='please enter correct email in lower case eg abcde@fgh.com';
   }
 });
